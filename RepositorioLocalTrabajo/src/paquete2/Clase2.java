09977595c70c040ffc2ac712e1b8a9ac5aae20dc
diego <diego@DESKTOP-1EBCB0U>
2024-02-13 19:12:35 +0100

    Segundo commit

--- RepositorioLocalTrabajo/src/paquete2/Clase2.java
+++ RepositorioLocalTrabajo/src/paquete2/Clase2.java
@@ -4,6 +4,8 @@ public class Clase2 {
 
 	public static void main(String[] args) {
 		System.out.println("Todo esto está en la rama1 ya :D");
+		System.out.println("Esto es para el segundo commit");
+		System.out.println(".");
 	}
 
 }
