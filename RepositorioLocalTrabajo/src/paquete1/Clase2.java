ead18d1841b23a04f0453a9658c79df884e667a3
diego <diego@DESKTOP-1EBCB0U>
2024-02-13 19:12:01 +0100

    Hacemos commit de la segunda clase.

---
+++ RepositorioLocalTrabajo/src/paquete1/Clase2.java
@@ -0,0 +1,11 @@
+package paquete1;
+
+public class Clase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Creando una segunda clase!");
+			System.out.println("Ahora debemos hacer un push");
+	}
+
+}
---
+++ RepositorioLocalTrabajo/src/paquete2/Clase2.java
@@ -0,0 +1,9 @@
+package paquete2;
+
+public class Clase2 {
+
+	public static void main(String[] args) {
+		System.out.println("Todo esto está en la rama1 ya :D");
+	}
+
+}
